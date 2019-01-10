29c2e4412c62c389422b4b14a64a6cca7804edf2
David Weston Webb <westwebb@gmail.com>
2019-01-10 11:14:29 -0500

    Create while_loop.php

---
+++ while_loop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>While Loops</title>
+</head>
+
+<body>
+
+<?php
+
+$counter = 0;
+while() {
+
+    echo $counter;
+    $counter++;
+    // $counter = $counter + 1;
+
+}
+
+
+?>
+
+</body>
+</html>
